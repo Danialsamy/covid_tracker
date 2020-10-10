cd3a6c23873f5cf19bf6dacaa57d766ee14a430e
RiperXNX <71544678+RiperXNX@users.noreply.github.com>
2020-10-10 16:37:21 +0200

    Add files via upload
    script che ottiene i dati

---
+++ data_script.py
@@ -0,0 +1,46 @@
+import os
+import requests
+import json
+import time
+
+#ad ogni avvio scarica i dati
+def getData():
+    os.system('cmd /c "curl https://raw.githubusercontent.com/pcm-dpc/COVID-19/master/dati-json/dpc-covid19-ita-andamento-nazionale-latest.json > dati.json"')
+
+getData()
+time.sleep(2)#aggiungere un controllo e togliere questa funzione
+data = json.load(open("dati.json"))
+
+
+# ritorna i valori sotto forma di stringa
+def data_gornaliera():
+    for i in data:
+        return(i['data'])
+def casi_totali():
+    for i in data:
+        return(i['totale_casi'])
+def nuovi_casi():
+    for i in data:
+        return(i['nuovi_positivi'])
+def variazione():
+    for i in data:
+        return(i['variazione_totale_positivi'])
+def deceduti():
+    for i in data:
+        return(i['deceduti'])
+def tamponi_effettuati():
+    for i in data:
+        return(i['tamponi'])
+def ricoverati():
+    for i in data:
+        return(i['ricoverati_con_sintomi'])
+def terapia_intensiva():
+    for i in data:
+        return(i['terapia_intensiva'])
+def isolamento_domiciliare():
+    for i in data:
+        return(i['isolamento_domiciliare'])
+def guariti():
+    for i in data:
+        return(i['dimessi_guariti'])
+
