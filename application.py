1622af758c7c3b1bcbe2f82bb45cd6f13fb92e07
mrdexter01 <danialsamy12@gmail.com>
2020-11-05 16:17:35 +0100

    prova

--- application.py
+++ application.py
@@ -2,10 +2,13 @@ from flask import Flask, render_template
 
 app = Flask('__name__')
 
+#pagina homepage
 @app.route('/')
 def index():
 	return render_template("index.html")
 
 if __name__ == '__main__':
    app.run(debug = True)
+
+
 
