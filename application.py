8f7491d09a0439df9e25a75f293b07e7c7faa112
mrdexter01 <danialsamy12@gmail.com>
2020-11-10 00:04:47 +0100

    prima versione completata

--- application.py
+++ application.py
@@ -1,14 +1,33 @@
 from flask import Flask, render_template
+import data_script
 
 app = Flask('__name__')
 
-
 @app.route('/')
 def index():
-	return render_template("index.html")
+	casi_totali = str(data_script.casi_totali())
+	deceduti = str(data_script.deceduti())
+	guariti = str(data_script.guariti())
+	nuovi_casi = str(data_script.nuovi_casi())
+	terapia_intensiva = str(data_script.terapia_intensiva())
+	isolamento_casa = str(data_script.isolamento_domiciliare())
+	ricoverati_con_sintomi = str(data_script.ricoverati_con_sintomi())
+	data_giornaliera = str(data_script.data_giornaliera())
+	totale_positivi= str(data_script.totale_positivi())
+	variazione_positivi= str(data_script.variazione_positivi())
+	return render_template("index.html", casi_totali = casi_totali, deceduti = deceduti, guariti= guariti, 
+		nuovi_casi = nuovi_casi, terapia_intensiva = terapia_intensiva, isolamento_casa= isolamento_casa,
+		ricoverati_con_sintomi = ricoverati_con_sintomi, data_giornaliera= data_giornaliera, 
+		totale_positivi= totale_positivi, variazione_positivi = variazione_positivi)
+
+@app.route('/mario')
+def mario():
+	return lol
+
+
 
 if __name__ == '__main__':
-   app.run(debug = True)
+	app.run(debug = True)
 
 
 
--- data_script.py
+++ data_script.py
@@ -8,12 +8,12 @@ def getData():
     os.system('cmd /c "curl https://raw.githubusercontent.com/pcm-dpc/COVID-19/master/dati-json/dpc-covid19-ita-andamento-nazionale-latest.json > dati.json"')
 
 getData()
-time.sleep(2)#aggiungere un controllo e togliere questa funzione
+time.sleep(2) #aggiungere un controllo e togliere questa funzione
 data = json.load(open("dati.json"))
 
 
 # ritorna i valori sotto forma di stringa
-def data_gornaliera():
+def data_giornaliera():
     for i in data:
         return(i['data'])
 def casi_totali():
@@ -22,7 +22,7 @@ def casi_totali():
 def nuovi_casi():
     for i in data:
         return(i['nuovi_positivi'])
-def variazione():
+def variazione_positivi():
     for i in data:
         return(i['variazione_totale_positivi'])
 def deceduti():
@@ -43,6 +43,12 @@ def isolamento_domiciliare():
 def guariti():
     for i in data:
         return(i['dimessi_guariti'])
-    
+def ricoverati_con_sintomi():
+	for i in data:
+		return(i['ricoverati_con_sintomi'])
+def totale_positivi():
+	for i in data:
+		return(i['totale_positivi'])
+
  #fine programma
 
